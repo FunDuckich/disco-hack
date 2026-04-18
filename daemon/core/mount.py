15c251e2a3bed2e515154ee48d49dd91173dab62
Moonrisefire <vitalijsibrin@gmail.com>
2026-04-18 10:35:54 +0400

    Continue of use NextCloud Api.

--- daemon/core/mount.py
+++ daemon/core/mount.py
@@ -28,39 +28,36 @@ async def mount():
     db_manager = DBManager(db_path)
     await db_manager.init_db()
 
-    cloud_api = None
-    cloud_type = None
-
-    while not cloud_api:
+    active_clients = {}
+    while not active_clients:
         yandex_token = await db_manager.get_config("yandex_token")
+        if yandex_token:
+            logging.info("Найден токен Яндекса. Добавляем в пул...")
+            active_clients["yandex"] = YandexDiskAsyncClient(token=yandex_token)
 
         nc_host = await db_manager.get_config("nc_host")
         nc_login = await db_manager.get_config("nc_login")
         nc_password = await db_manager.get_config("nc_password")
+        if nc_host and nc_login and nc_password:
+            logging.info("Найдены данные Nextcloud. Добавляем в пул...")
+            active_clients["nextcloud"] = NextcloudAsyncClient(nc_host, nc_login, nc_password)
 
-        if yandex_token:
-            logging.info("Найден токен Яндекса. Инициализация...")
-            cloud_api = YandexDiskAsyncClient(token=yandex_token)
-            cloud_type = "yandex"
+        if not active_clients:
+            logging.info("Нет подключенных дисков. Ожидание авторизации...")
+            await asyncio.sleep(5)
 
-        elif nc_host and nc_login and nc_password:
-            logging.info("Найдены данные Nextcloud. Инициализация...")
-            cloud_api = NextcloudAsyncClient(nc_host, nc_login, nc_password)
-            cloud_type = "nextcloud"
+    for cloud_type, client in active_clients.items():
+        try:
+            logging.info(f"Синхронизация структуры для {cloud_type.upper()}...")
+            cloud_files = await client.get_all_files_flat()
 
-        else:
-            logging.info("Ожидание авторизации через GUI...")
-            await asyncio.sleep(5)  # Ждем 5 секунд и проверяем базу снова
+            if cloud_files:
+                await import_cloud_to_db(db_manager, cloud_files, cloud_type=cloud_type)
+                logging.info(f"✅ {cloud_type.upper()} синхронизирован.")
+        except Exception as e:
+            logging.error(f" Ошибка синхронизации {cloud_type}: {e}")
 
-    try:
-        logging.info(f"Синхронизация дерева файлов для {cloud_type}...")
-        cloud_files = await cloud_api.get_all_files_flat()
-        if cloud_files:
-            await import_cloud_to_db(db_manager, cloud_files, cloud_type=cloud_type)
-    except Exception as e:
-        logging.error(f"Ошибка при первичной синхронизации: {e}")
-
-    vfs = CloudFusionVFS(db_manager, cloud_api)
+    vfs = CloudFusionVFS(db_manager, active_clients)
     fuse_options = set(pyfuse3.default_options)
     fuse_options.add('allow_other')
 
